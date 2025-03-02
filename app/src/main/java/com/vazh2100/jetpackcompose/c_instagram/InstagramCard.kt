4e918b83094c7f7f0f94bd770b307aed56f4d233
vazh2100 <vazh2100@list.ru>
2025-03-02 20:14:46 +0300

    Jetpack Basics: InstagramCard 1

---
+++ app/src/main/java/com/vazh2100/jetpackcompose/c_instagram/InstagramCard.kt
@@ -0,0 +1,48 @@
+package com.vazh2100.jetpackcompose.c_instagram
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun InstagramCard(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.height(500.dp).padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth().height(100.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(modifier = Modifier.size(30.dp).background(color = Color.Red))
+            Part("6,960", "Posts")
+            Part("436 M", "Followers")
+            Part("76", "Following")
+        }
+    }
+}
+
+@Composable
+fun Part(text1: String, text2: String, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(text1)
+        Text(text2)
+    }
+}
