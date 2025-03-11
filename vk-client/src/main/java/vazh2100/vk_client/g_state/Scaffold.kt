f23673ecd8894c3e3e8ea9ab8915e7990e98e223
vazh2100 <vazh2100@list.ru>
2025-03-11 15:14:27 +0300

    Material Components: FAB and SnackBar

--- vk-client/src/main/java/vazh2100/vk_client/g_state/Scaffold.kt
+++ vk-client/src/main/java/vazh2100/vk_client/g_state/Scaffold.kt
@@ -1,38 +1,71 @@
 package vazh2100.vk_client.g_state
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) = Scaffold(bottomBar = {
-    NavigationBar {
-        var selectedItem by remember { mutableStateOf(BottomNavigationItem.HOME) }
+fun MainScreen(modifier: Modifier = Modifier) {
+    val snackBarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+    var fabVisibility by remember { mutableStateOf(true) }
+    Scaffold(
+        snackbarHost = { SnackbarHost(snackBarHostState) },
+        bottomBar = {
+            NavigationBar {
+                var selectedItem by remember { mutableStateOf(BottomNavigationItem.HOME) }
 
-        BottomNavigationItem.entries.forEach { item ->
-            NavigationBarItem(
-                selected = item == selectedItem,
-                onClick = { selectedItem = item },
-                icon = { Icon(imageVector = item.icon, contentDescription = null) },
-                label = { Text(stringResource(item.title)) },
-                colors = NavigationBarItemDefaults.colors(
-                    unselectedIconColor = MaterialTheme.colorScheme.onSecondary,
-                    unselectedTextColor = MaterialTheme.colorScheme.onSecondary,
-                )
+                BottomNavigationItem.entries.forEach { item ->
+                    NavigationBarItem(
+                        selected = item == selectedItem,
+                        onClick = { selectedItem = item },
+                        icon = { Icon(item.icon, null) },
+                        label = { Text(stringResource(item.title)) },
+                        colors = NavigationBarItemDefaults.colors(
+                            unselectedIconColor = MaterialTheme.colorScheme.onSecondary,
+                            unselectedTextColor = MaterialTheme.colorScheme.onSecondary,
+                        )
+                    )
+                }
+            }
+        },
+        floatingActionButton = {
+            if (fabVisibility) FloatingActionButton(
+                onClick = {
+                    scope.launch {
+                        val action = snackBarHostState.showSnackbar(
+                            message = "This is snack bar",
+                            actionLabel = "Hide FAB",
+                            duration = SnackbarDuration.Long,
+                        )
+                        if (action == SnackbarResult.ActionPerformed) fabVisibility = false
+                    }
+                },
+                content = { Icon(Icons.Filled.Favorite, null) },
             )
-        }
-    }
-}, content = { it })
+        },
+        content = { it },
+    )
+}
 
 
