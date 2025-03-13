a59394aa735db9e021c0ee847e74191034156bb2
vazh2100 <vazh2100@list.ru>
2025-03-13 15:10:14 +0300

    State: ViewModel

--- vk-client/src/main/java/vazh2100/vk_client/MainActivity.kt
+++ vk-client/src/main/java/vazh2100/vk_client/MainActivity.kt
@@ -10,16 +10,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import vazh2100.vk_client.h_click_listeners.MainScreen
+import androidx.lifecycle.ViewModelProvider
+import vazh2100.vk_client.i_view_model.MainScreen
+
+import vazh2100.vk_client.i_view_model.MainViewModel
 import vazh2100.vk_client.theme.VkTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel = ViewModelProvider(this)[MainViewModel::class]
         setContent {
             VkTheme {
                 Box(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background).padding(8.dp)) {
-                    MainScreen()
+                    MainScreen(viewModel)
                 }
             }
         }
---
+++ vk-client/src/main/java/vazh2100/vk_client/i_view_model/MainScreen.kt
@@ -0,0 +1,61 @@
+package vazh2100.vk_client.i_view_model
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import vazh2100.vk_client.g_state.BottomNavigationItem
+import vazh2100.vk_client.h_click_listeners.entities.FeedPost
+import vazh2100.vk_client.i_view_model.widgets.PostCard
+
+@Composable
+fun MainScreen(
+    viewModel: MainViewModel,
+    modifier: Modifier = Modifier,
+) {
+    val feedPost = viewModel.feedPost.observeAsState(FeedPost())
+
+    Scaffold(
+        modifier = modifier,
+        bottomBar = {
+            NavigationBar {
+                var selectedItem by remember { mutableStateOf(BottomNavigationItem.HOME) }
+
+                BottomNavigationItem.entries.forEach { item ->
+                    NavigationBarItem(
+                        selected = item == selectedItem,
+                        onClick = { selectedItem = item },
+                        icon = { Icon(item.icon, null) },
+                        label = { Text(stringResource(item.title)) },
+                        colors = NavigationBarItemDefaults.colors(
+                            unselectedIconColor = MaterialTheme.colorScheme.onSecondary,
+                            unselectedTextColor = MaterialTheme.colorScheme.onSecondary,
+                        )
+                    )
+                }
+            }
+        },
+        content = { paddingValues ->
+            PostCard(
+                modifier = Modifier.padding(paddingValues),
+                feedPost = feedPost,
+                onViewsPress = viewModel::onViewsPress,
+                onSharesPress = viewModel::onSharesPress,
+                onCommentsPress = viewModel::onCommentsPress,
+                onLikesPress = viewModel::onLikesPress,
+            )
+        },
+    )
+}
