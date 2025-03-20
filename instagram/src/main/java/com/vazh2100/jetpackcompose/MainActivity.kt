687989abbce2b14421fee454c7c0f71e33d747d0
vazh2100 <vazh2100@list.ru>
2025-03-20 11:20:05 +0300

    Activity Result API: Activity

--- instagram/src/main/java/com/vazh2100/jetpackcompose/MainActivity.kt
+++ instagram/src/main/java/com/vazh2100/jetpackcompose/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.vazh2100.jetpackcompose
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -27,5 +31,16 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+
+
+        registerForActivityResult(ForStringContract) { string -> println("string") }.launch(Intent(""))
+        ActivityResultContracts.RequestPermission()
+    }
+
+    // ActivityResultApi
+    object ForStringContract : ActivityResultContract<Intent, String?>() {
+        override fun createIntent(context: Context, input: Intent): Intent = input
+        override fun parseResult(resultCode: Int, intent: Intent?): String? = intent?.getStringExtra("key")
     }
 }
